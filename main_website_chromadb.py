94e2b77c5d32f5e9be96a15dcb15e126d9b1fd64
Pratham-Sharda <144925900+Pratham-Sharda@users.noreply.github.com>
2024-06-01 23:52:19 +0530

    Update main_website_chromadb.py

--- main_website_chromadb.py
+++ main_website_chromadb.py
@@ -11,14 +11,44 @@ from langchain_chroma import Chroma
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
+
+def fetch_sitemap(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Ensure we notice bad responses
+    return response.content
+
+def parse_sitemap(xml_content):
+    soup = BeautifulSoup(xml_content, 'lxml-xml')  # Use 'lxml' parser
+    sitemap_urls = [loc.text for loc in soup.find_all('loc')]
+    return sitemap_urls
+
+def get_all_urls(sitemap_url):
+    all_urls = []
+    urls_to_process = [sitemap_url]
+
+    while urls_to_process:
+        current_url = urls_to_process.pop()
+        content = fetch_sitemap(current_url)
+        urls = parse_sitemap(content)
+
+        for url in urls:
+            if url.endswith('.xml'):
+                urls_to_process.append(url)
+            else:
+                all_urls.append(url)
+
+    return all_urls
+    
 class ChatBot():
     load_dotenv()
-    URL_L=['https://urbanemissions.info/','https://urbanemissions.info/blog-pieces/whats-polluting-delhis-air/','https://urbanemissions.info/india-apna/'
-            'https://urbanemissions.info/tools/','https://urbanemissions.info/tools/vapis/','https://urbanemissions.info/tools/aqi-calculator','https://urbanemissions.info/tools/atmos',
-            'https://urbanemissions.info/tools/wrf-configuration-notes/','https://urbanemissions.info/india-air-quality/india-ncap-review/#sim-40-2021',
-            'https://urbanemissions.info/blog-pieces/airquality-smog-towers-crazy/','https://urbanemissions.info/blog-pieces/delhi-diwali-2017-understanding-emission-loads/',
-            'https://urbanemissions.info/blog-pieces/resources-how-to-access-aqdata-in-india/','https://urbanemissions.info/india-emissions-inventory/emissions-in-india-open-agricultural-forest-fires/']
-    loader = UnstructuredURLLoader(urls=URL_L)
+    # URL_L=['https://urbanemissions.info/','https://urbanemissions.info/blog-pieces/whats-polluting-delhis-air/','https://urbanemissions.info/india-apna/'
+    #         'https://urbanemissions.info/tools/','https://urbanemissions.info/tools/vapis/','https://urbanemissions.info/tools/aqi-calculator','https://urbanemissions.info/tools/atmos',
+    #         'https://urbanemissions.info/tools/wrf-configuration-notes/','https://urbanemissions.info/india-air-quality/india-ncap-review/#sim-40-2021',
+    #         'https://urbanemissions.info/blog-pieces/airquality-smog-towers-crazy/','https://urbanemissions.info/blog-pieces/delhi-diwali-2017-understanding-emission-loads/',
+    #         'https://urbanemissions.info/blog-pieces/resources-how-to-access-aqdata-in-india/','https://urbanemissions.info/india-emissions-inventory/emissions-in-india-open-agricultural-forest-fires/']
+    initial_sitemap_url = 'https://urbanemissions.info/sitemap.xml'
+    all_extracted_urls = get_all_urls(initial_sitemap_url)
+    loader = UnstructuredURLLoader(urls=all_extracted_urls)
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
     docs = text_splitter.split_documents(documents)
@@ -52,7 +82,7 @@ class ChatBot():
     repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
     llm = HuggingFaceHub(
     repo_id=repo_id, 
-    model_kwargs={"temperature": 0.8, "top_k": 25}, 
+    model_kwargs={"temperature": 0.8, "top_k": 50}, 
     huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_TOKEN')
     )
 
