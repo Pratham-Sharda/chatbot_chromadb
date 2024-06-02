391f2158d1b083e4554737d37fed16b14534f011
Pratham-Sharda <144925900+Pratham-Sharda@users.noreply.github.com>
2024-06-02 13:42:20 +0530

    Update main_website_chromadb.py

--- main_website_chromadb.py
+++ main_website_chromadb.py
@@ -84,8 +84,8 @@ class ChatBot():
     repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
     llm = HuggingFaceHub(
     repo_id=repo_id, 
-    model_kwargs={"temperature": 0.8, "top_k": 50}, 
-    huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_TOKEN')
+    model_kwargs={"temperature": 0.8, "top_k": 50}, huggingfacehub_api_token = st.secrets["HUGGINGFACE_API_TOKEN"]
+    
     )
 
     from langchain import PromptTemplate
